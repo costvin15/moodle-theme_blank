0e1c20d3995455a50c746257c982c3b997570ae3
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-05-24 16:07:27 -0300

    feature(config): Theme configuration improved

--- config.php
+++ config.php
@@ -1,4 +1,35 @@
 <?php
 
+// Theme's name.
 $THEME->name = "blank";
-$THEME->parents = [];
+// Array containing the names of the CSS stylesheets.
+$THEME->sheets = array();
+// Array containing the names of the CSS stylesheets to include for the TinyMCE text editor content area.
+$THEME->editor_sheets = array();
+// Array containing maps to the different layout types to different layout files.
+$THEME->layouts = array(
+    "base" => array(
+        "file" => "columns2.php",
+        "regions" => array(),
+    ),
+    "login" => array(
+        "file" => "login.php",
+        "regions" => array(),
+    ),
+    "maintenance" => array(
+        "file" => "maintenance.php",
+        "regions" => array(),
+    ),
+);
+// Defines the themes that the theme will extend.
+$THEME->parents = array();
+// Defines support for docking blocks.
+$THEME->enable_dock = false;
+// Defines a function to post process the css tree.
+$THEME->csstreepostprocessor = "";
+// Array containing a list of YUI css files to load.
+$THEME->yuicssmodules = array();
+// Defines support to customize any core renderers.
+$THEME->rendererfactory = "theme_overridden_renderer_factory";
+// Defines a comma separated list of block types that cannot be deleted in this theme.
+$THEME->undeletableblocktypes = "";
