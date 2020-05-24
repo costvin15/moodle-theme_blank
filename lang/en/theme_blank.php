d50f56487d1a5a4b74343bb260cb66624ca601f4
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-05-24 20:02:25 -0300

    feature(scss): Bootstrap grid imported

--- lang/en/theme_blank.php
+++ lang/en/theme_blank.php
@@ -1,3 +1,7 @@
 <?php
 
 $string["pluginname"] = "Blank";
+$string["configtitle"] = "Blank";
+$string["preset"] = "Theme Preset";
+$string["preset_desc"] = "Pick a preset to broadly change the look of the theme.";
+$string["generalsettings"] = "General settings";
--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
-$plugin->version = "2020052401";
+$plugin->version = "2020052405";
 $plugin->requires = "2019052000";
 $plugin->component = "theme_blank";
