97e71da95726be663b43c680a1c71e7c1de82bd6
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-05-24 00:18:47 -0300

    Mandatory files for a moodle theme implemented

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+$THEME->name = "blank";
+$THEME->parents = [];
---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+
+$plugin->version = "2020052400";
+$plugin->requires = "2019052000";
+$plugin->component = "theme_blank";
