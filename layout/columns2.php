f0e5005f745dc764eeea0398a71b212cd51f971a
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-05-25 17:59:16 -0300

    feature(columns2): Columns2 improved

--- layout/columns2.php
+++ layout/columns2.php
@@ -1,12 +1,25 @@
 <?php
 
-global $OUTPUT;
+global $OUTPUT, $SITE;
 
+$sitename = format_string(
+    $SITE->shortname,
+    true,
+    array(
+        "context" => context_course::instance(SITEID),
+        "escape" => false,
+    ),
+);
+$blockshtml = $OUTPUT->blocks("side-pre");
+$hasblocks = strpos($blockshtml, "data-block=") !== false;
 $extraclasses = array();
 $bodyattributes = $OUTPUT->body_attributes($extraclasses);
 
 $templatecontext = array(
+    "sitename" => $sitename,
     "output" => $OUTPUT,
+    "sidepreblocks" => $blockshtml,
+    "hasblocks" => $hasblocks,
     "body_attributes" => $bodyattributes
 );
 
