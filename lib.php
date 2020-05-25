6a422ccca7cda29a78221a2853cae081bc0d6225
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-05-24 21:24:07 -0300

    feature(scss): Bootstrap and near implemented

--- lib.php
+++ lib.php
@@ -7,11 +7,11 @@ function theme_blank_get_main_scss_content($theme) {
     $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
 
     if ($filename == "default.scss") {
-        $scss .= file_get_contents("$CFG->dirroot/theme/blank/scss/default.scss");
+        $scss .= file_get_contents("$CFG->dirroot/theme/blank/scss/preset/default.scss");
     } else if ($filename == "plain.scss") {
         $scss .= file_get_contents("$CFG->dirroot/theme/blank/scss/plain.scss");
     } else {
-        $scss .= file_get_contents("$CFG->dirroot/theme/blank/scss/default.scss");
+        $scss .= file_get_contents("$CFG->dirroot/theme/blank/scss/preset/default.scss");
     }
 
     return $scss;
